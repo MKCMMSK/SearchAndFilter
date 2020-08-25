815296610001412dadef8c8f7ab1750afcffc8af
MShaoK <53098934+MShaoK@users.noreply.github.com>
2020-08-24 23:53:43 -0700

    added events and categories controllers

---
+++ SearchAndFilter/SearchAndFilter/Data/ICategoryRepo.cs
@@ -0,0 +1,14 @@
+﻿using SearchAndFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchAndFilter.Data
+{
+    public interface ICategoryRepo
+    {
+        IEnumerable<Category> GetCategories();
+        Category GetCategoryById(int id);
+    }
+}
